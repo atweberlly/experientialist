2d484954e12e66fb2d94c98a1d5d12d9263dc37e
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2022-07-11 21:57:16 +0800

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' && {
+      content: ['./index.html', './src/**/*.vue'],
+      defaultExtractor: (content) => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || []
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || []
+        return broadMatches.concat(innerMatches)
+      },
+      keyframes: true,
+      variables: true,
+    },
+    'postcss-import': {},
+    'postcss-sort-media-queries': {},
+    tailwindcss: {},
+  },
+}
