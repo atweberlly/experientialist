502b838b572a4c253b3a6e38430c8cdf8f1d1990
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-07-23 15:48:33 +0800

    Sort class names

--- src/views/ComingSoon.vue
+++ src/views/ComingSoon.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="min-h-screen min-w-xs bg-beach bg-cover bg-center bg-no-repeat antialiased md:bg-top lg:bg-top-16">
-    <div class="absolute inset-0 min-w-xs bg-secondary-500/50 md:hidden"
-      aria-hidden="true"
-      aria-label="Fullscreen background overlay"></div>
+    <div class="absolute inset-0 min-w-xs bg-secondary-500/50 md:hidden" aria-hidden="true" aria-label="Fullscreen background overlay"></div>
     <div class="relative grid h-screen grid-cols-1 items-center py-8 text-white md:grid-cols-2 md:py-0">
       <div>
-        <img class="mx-auto w-56 lg:w-64"
-          src="/images/experientialist-logotype-alt.png"
-          alt="Experientialist" />
+        <img class="mx-auto w-56 lg:w-64" src="/images/experientialist-logotype-alt.png" alt="Experientialist" />
       </div>
       <div class="md:relative md:h-full">
-        <div class="inset-0 hidden bg-secondary-500/50 md:absolute md:block"
-          aria-hidden="true"
-          aria-label="Half screen background overlay"></div>
+        <div class="inset-0 hidden bg-secondary-500/50 md:absolute md:block" aria-hidden="true" aria-label="Half screen background overlay"></div>
         <div class="md:relative md:flex md:h-full md:flex-col md:justify-center">
           <h1 class="text-center text-5xl uppercase tracking-tight">
             Great things
@@ -20,17 +14,14 @@
             are coming
           </h1>
           <div class="mt-4 flex justify-center">
-            <button @click="showLearnMore(true);"
-              class="mx-auto inline-block bg-primary-300 px-4 py-3">Learn more</button>
+            <button class="mx-auto inline-block bg-primary-300 px-4 py-3" @click="showLearnMore(true)">Learn more</button>
           </div>
           <div class="mt-4 flex items-center justify-center gap-4">
-            <a href="https://www.facebook.com/ExperientialistTravel"
-              target="_blank">
+            <a href="https://www.facebook.com/ExperientialistTravel" target="_blank">
               <span class="sr-only">Facebook</span>
               <span class="fab fa-facebook text-2xl"></span>
             </a>
-            <a href="https://www.instagram.com/experientialist_travel/"
-              target="_blank">
+            <a href="https://www.instagram.com/experientialist_travel/" target="_blank">
               <span class="sr-only">Instagram</span>
               <span class="fab fa-instagram text-2xl"></span>
             </a>
@@ -38,72 +29,34 @@
         </div>
       </div>
     </div>
-    <div id="learn-more"
-      class="hidden w-full h-full bg-opacity-80 top-0 fixed sticky-0">
-      <div class="2xl:mx-auto py-48 px-4 md:px-28 flex justify-center items-center">
-        <div
-          class="w-96 md:w-auto bg-secondary-500 relative flex flex-col justify-center items-center py-16 px-4 md:px-24 xl:py-24 xl:px-36">
+    <div class="sticky-0 fixed top-0 hidden h-full w-full bg-opacity-80" id="learn-more">
+      <div class="flex items-center justify-center py-48 px-4 md:px-28 2xl:mx-auto">
+        <div class="relative flex w-96 flex-col items-center justify-center bg-secondary-500 py-16 px-4 md:w-auto md:px-24 xl:py-24 xl:px-36">
           <div role="logo">
-            <img class="mx-auto w-56 lg:w-64"
-              src="/images/experientialist-logotype-alt.png"
-              alt="Experientialist" />
+            <img class="mx-auto w-56 lg:w-64" src="/images/experientialist-logotype-alt.png" alt="Experientialist" />
           </div>
           <div class="mt-12">
-            <h1 role="main"
-              class="text-5xl font-display text-secondary-50 lg:text-6xl font-bold text-center">Our Blog is Launching
-              Soon.</h1>
+            <h1 role="main" class="text-center font-display text-5xl font-bold text-secondary-50 lg:text-6xl">Our Blog is Launching Soon.</h1>
           </div>
           <div class="mt">
-            <p class="mt-8 sm:w-96 text-lg font-light text-secondary-100 text-center">Sign up for our latest blog posts
-              and to be the first to know when we go live! </p>
+            <p class="mt-8 text-center text-lg font-light text-secondary-100 sm:w-96">Sign up for our latest blog posts and to be the first to know when we go live!</p>
           </div>
-          <div class="flex grid-cols-2 mt-16 justify-center">
-            <div>
-              <label class="relative block">
-                <input
-                  class="placeholder:italic placeholder:text-secondary-100 text-secondary-200 block bg-white w-full py-4 pl-2 md:pl-4 pr-0 sm:pr-8 md:pr-16 shadow-sm text-sm focus:outline-none focus:border-primary-300 focus:ring-primary-300 focus:ring-1"
-                  placeholder="Enter your email here*"
-                  type="email"
-                  v-model="email" />
-              </label>
-            </div>
-            <div>
-              <button
-                class="inline-block bg-primary-300 hover:bg-primary-500 px-4 py-4 text-sm capitalize text-secondary-50"
-                @click="saveEmail()">
-                Notify Me!
-                <svg xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-7 hidden md:inline-block"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="1">
-                  <path stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                </svg>
-              </button>
-            </div>
+          <div class="mt-16 flex grid-cols-2 items-center justify-center">
+            <label class="relative block">
+              <input class="block w-full bg-white py-4 pl-2 pr-0 text-sm text-secondary-200 shadow-sm placeholder:italic placeholder:text-secondary-100 focus:border-primary-300 focus:outline-none focus:ring-1 focus:ring-primary-300 sm:pr-8 md:pl-4 md:pr-16" type="email" placeholder="Enter your email here*" v-model="email" />
+            </label>
+            <button class="inline-flex items-center gap-1 bg-primary-300 px-4 py-4 text-sm capitalize text-secondary-50 hover:bg-primary-500" @click="saveEmail()">
+              <span>Notify me!</span>
+              <svg class="hidden h-5 w-5 md:inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m17 8 4 4m0 0-4 4m4-4H3" />
+              </svg>
+            </button>
           </div>
           <p class="mt-2 hidden text-secondary-50" id="success-msg">Thanks for submitting!</p>
-          <button @click="showLearnMore(true)"
-            class="text-white absolute top-8 right-8 hover:text-secondary-50"
-            aria-label="close">
-            <svg width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path d="M18 6L6 18"
-                stroke="currentColor"
-                stroke-width="1.66667"
-                stroke-linecap="round"
-                stroke-linejoin="round"></path>
-              <path d="M6 6L18 18"
-                stroke="currentColor"
-                stroke-width="1.66667"
-                stroke-linecap="round"
-                stroke-linejoin="round"></path>
+          <button class="absolute top-8 right-8 text-white hover:text-secondary-50" @click="showLearnMore(true)">
+            <span class="sr-only">Close</span>
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.667" d="M18 6 6 18M6 6l12 12" />
             </svg>
           </button>
         </div>
@@ -113,40 +66,41 @@
 </template>
 
 <script>
-import axios from "axios"
+import axios from 'axios'
+
 export default {
   mounted: () => {
     document.title = 'Coming Soon'
   },
-  data () {
+  data() {
     return {
       email: '',
-      newsletterData: []
-    };
+      newsletterData: [],
+    }
   },
   async created() {
     try {
       const res = await axios.get(`http://localhost:3000/emails`)
       this.emails = res.data
-
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.log(error)
     }
   },
   methods: {
-    showLearnMore(flag) {
-      let menu = document.getElementById('learn-more')
+    showLearnMore() {
+      const menu = document.getElementById('learn-more')
       menu.classList.toggle('hidden')
     },
-    async saveEmail(){
-      let message = document.getElementById('success-msg')
+    async saveEmail() {
+      const message = document.getElementById('success-msg')
       const res = await axios.post(`http://localhost:3000/emails`, {
         email: this.email,
-      });
+      })
       this.emails = [...this.emails, res.data]
-      this.email = ""
+      this.email = ''
       message.classList.toggle('hidden')
-    }
-  }
+    },
+  },
 }
 </script>
