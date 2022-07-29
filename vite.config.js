d0b7ae02e401ca396419dfd7a0e2f9784cb3a3ea
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-07-29 21:47:41 +0800

    Update config files

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    port: '8080',
-  },
-  build: {
-    rollupOptions: {
-      input: { index: './index.html' },
-    },
+    open: true,
   },
 })
