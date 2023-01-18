0126ef9fa413b1f37f7f2e16cc39f2897ae58b61
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-01-18 16:11:58 +0800

    Delete Vue-related files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
-import './styles/main.css'
-
-const app = createApp(App)
-
-app.use(router)
-app.mount(document.getElementById('app'))
